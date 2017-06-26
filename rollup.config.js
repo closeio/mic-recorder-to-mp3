aba9bc578131d122eea57465afcd0f3167998008
Vitor Buzinaro <vitor@close.io>
2017-06-25 23:55:39 -0300

    add minified version

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,20 @@
-const rollup = require('rollup');
-const nodeResolve = require('rollup-plugin-node-resolve');
-const commonjs = require('rollup-plugin-commonjs');
-const babel = require('rollup-plugin-babel');
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
 
-rollup
-  .rollup({
-    entry: 'src/mic-recorder.js',
-    plugins: [
-      nodeResolve({ jsnext: true, main: true }),
-      commonjs({ include: 'node_modules/**' }),
-      babel({ exclude: 'node_modules/**' }),
-    ],
-  })
-  .then(bundle => bundle.write({
-    format: 'umd',
-    dest: 'dist/index.js',
-    sourceMap: true,
-    moduleName: 'MicRecorder',
-    useStrict: false,
-  }))
-  .catch(console.error.bind(console));
+export default {
+  entry: 'src/mic-recorder.js',
+  plugins: [
+    nodeResolve({ jsnext: true, main: true }),
+    commonjs({ include: 'node_modules/**' }),
+    babel({ exclude: 'node_modules/**' }),
+    !!process.env.minify && uglify(),
+  ],
+  format: 'umd',
+  dest: `dist/index${process.env.minify ? '.min' : ''}.js`,
+  sourceMap: true,
+  moduleName: 'MicRecorder',
+  useStrict: false,
+}
   
