0af156f0dc306993cc5391b834c77b172435b4aa
Ben Mackerras <ben@coursesuite.com.au>
2018-11-20 12:00:34 +1100

    Device Id to be set by user, null by default
    This would allow a user to specify a device to use, or leave it blank and get prompted to select from all available audio devices

--- src/mic-recorder.js
+++ src/mic-recorder.js
@@ -12,7 +12,7 @@ class MicRecorder {
       // the begining of the recording. It also helps to remove the mouse
       // "click" sound from the output mp3 file.
       startRecordingAt: 300,
-      deviceId: 'default',
+      deviceId: null,
     };
 
     this.activeStream = null;
@@ -90,8 +90,10 @@ class MicRecorder {
     this.config.sampleRate = this.context.sampleRate;
     this.lameEncoder = new Encoder(this.config);
 
+    const audio = this.config.deviceId ? { deviceId: { exact: this.config.deviceId } } : true;
+
     return new Promise((resolve, reject) => {
-      navigator.mediaDevices.getUserMedia({ audio: true })
+      navigator.mediaDevices.getUserMedia({ audio: audio })
         .then(stream => {
           this.addMicrophoneListener(stream);
           resolve(stream);
